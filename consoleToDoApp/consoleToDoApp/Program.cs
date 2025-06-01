01942a1a084c3437876a2c7248b69c720b101c67
tunc3r93 <178416938+tunc3r93@users.noreply.github.com>
2025-06-01 13:56:55 +0200

    feat: add main menu structure for console ToDo app

---
+++ consoleToDoApp/consoleToDoApp/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace consoleToDoApp
+{
+    internal class Program
+    {
+        static List<string> todoList = new List<string>();
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("Task List Manager");
+                Console.WriteLine("--------------------");
+                Console.WriteLine("1. Show tasks");
+                Console.WriteLine("2. Add a task");
+                Console.WriteLine("3. Delete a task");
+                Console.WriteLine("4. Exit");
+                Console.Write("Choose an option: ");
+                string choice = Console.ReadLine();
+
+                switch (choice)
+                {
+                    case "1":
+                        //ShowTasks();
+                        break;
+                    case "2":
+                        //AddTask();
+                        break;
+                    case "3":
+                        //DeleteTask();
+                        break;
+                    case "4":
+                        return;
+                    default:
+                        Console.WriteLine("Invalid input. Press any key to continue...");
+                        Console.ReadKey();
+                        break;
+                }
+
+            }
+        }
+    }
+}
